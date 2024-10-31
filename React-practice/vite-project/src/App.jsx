6f2b50c5ce977f2c228a3fc11ee3dc3839caf7bd
E0min <bbok4yo@gmail.com>
2024-10-31 09:14:09 +0900

    xm

--- React-practice/vite-project/src/App.jsx
+++ React-practice/vite-project/src/App.jsx
@@ -1,54 +1,35 @@
-import "./App.css";
-import { useState, useRef, useEffect } from "react";
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  switch(action.type){
+    case "plus":
+      return state+1;
+    case "minus":
+      return state-1;
+  }
+ 
+}
 
 function App() {
-  const [form, setForm] = useState({
-    name: "",
-    birth: "",
-    country: "",
-  });
+  const [count,dispatch] = useReducer(reducer,0);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
-  };
+  const onCountUp = ()=>{
+    dispatch({type:"plus"});
+  }
 
-  useEffect(() => {
-    console.dir(`${form.name}`);
-  }, [form.name]);
-  return (
-    <div>
-      <h1>{form.name}</h1>
-      <h1>{form.birth}</h1>
-      <h1>{form.country}</h1>
+  const onCountDown = ()=>{
+    dispatch({type:"minus"});
+  }
 
-      <div>
-        <input
-          type="text"
-          name="name"
-          placeholder="이름"
-          onChange={handleChange}
-          value={form.name}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          name="birth"
-          placeholder="생일"
-          onChange={handleChange}
-          value={form.birth}
-        />
-      </div>
-      <div>
-        <select name="country" value={form.country} onChange={handleChange}>
-          <option value="한국">한국</option>
-          <option value="미국">미국</option>
-          <option value="영국">영국</option>
-        </select>
-      </div>
-    </div>
-  );
+  return(
+    <>
+    <h1>{count}</h1>
+    <button onClick={onCountUp}>+</button>
+    <button onClick={onCountDown}>-</button>
+
+    </>
+  )
+    
 }
 
 export default App;
